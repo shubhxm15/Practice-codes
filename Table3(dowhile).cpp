0461950f78755ac4f7c6f11aab8093d6232aadc5
Shubham <149764503+shubhxm15@users.noreply.github.com>
2024-01-28 20:54:13 +0530

    Add files via upload

---
+++ Table3(dowhile).cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i=1;
+    int x=9;
+   do{
+    cout<<x<<" * "<<i<<" = "<<x*i<<endl;
+    i++;
+   }while(i<=10);
+
+    return 0;
+}       
